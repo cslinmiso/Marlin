c884da56cc2f6c2dbfe8e62737c51ef2e461309f
weihsuehl <weihsuehl@yahooinc.com>
2023-03-20 03:50:57 +0800

    high speed connection, https://www.reddit.com/r/octoprint/comments/txud3q/higher_connection_speed_better_quality/

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -113,7 +113,7 @@
  *
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
-#define BAUDRATE 250000 // TL
+#define BAUDRATE 1000000 // TL
 //#define BAUD_RATE_GCODE     // Enable G-code M575 to set the baud rate
 
 /**
